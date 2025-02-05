ca576483f6042b52b8afb3fc6e48056518a073d7
Najath Mohammed <najathabdulla@gmail.com>
2025-02-06 01:15:14 +0530

    created React frontend to EMS

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ListEmployeeComponent from './components/ListEmployeeComponent'
+import HeaderComponent from './components/HeaderComponent'
+import FooterComponent from './components/FooterCOmponent'
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import EmployeeComponent from './components/EmployeeComponent'
+function App() {
+
+
+  return (
+  <>
+  <BrowserRouter>
+  <HeaderComponent/>
+    <Routes>
+        <Route path='/' element ={ <ListEmployeeComponent/> }></Route>
+        <Route path='/employees' element ={ <ListEmployeeComponent/> }></Route>
+        <Route path='/add-employee' element ={ <EmployeeComponent/> }></Route>
+        <Route path='/edit-employee/:id' element ={ <EmployeeComponent/> }></Route>
+    </Routes>
+  <FooterComponent/>
+  </BrowserRouter>
+  </>
+  )
+}
+
+export default App
